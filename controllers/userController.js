ae68f3d53382fb4a085c69ced4a7086eb04fcd30
nafis-tech <nafiezlela9@gmail.com>
2021-10-05 15:26:05 +0700

    P2-7-register-nafis

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,5 +18,34 @@ module.exports = {
             // res.status(200).send({dataUser: result[0], token})
             res.status(200).send(result)
         }) 
+    },
+    
+    getRegister: (req, res) => {
+        let getQuery = `select * from user;`
+
+        db.query(getQuery, (err, result) => {
+            if (err) {
+                console.log(err)
+                res.status(400).send(err)
+            }
+
+            res.status(200).send(result)
+        })
+    },
+
+    addUser : (req, res) => {
+        const { username, email, password, fullname, gender, address, age, role, profile_picture, verify} = req.body
+
+        let addQuery = `insert into user (username, email, password, fullname, gender, address, age, role, profile_picture, verify)
+        values (${db.escape(username)}, ${db.escape(email)}, ${db.escape(password)}, ${db.escape(fullname)}, ${db.escape(gender)}, ${db.escape(address)}, ${db.escape(age)}, ${db.escape(role)}, ${db.escape(profile_picture)}, ${db.escape(verify)});`
+
+        db.query(addQuery, (err, result) => {
+            if (err) {
+                console.log(err)
+                res.status(400).send(err)
+            }
+
+            res.status(200).send(result)
+        })
     }
 }
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,5 +3,7 @@ const router = require('express').Router()
 const { userController } = require('../controllers')
 
 router.post('/login', userController.login)
+router.get('/regis', userController.getRegister)
+router.post('/addUser', userController.addUser)
 
 module.exports = router
