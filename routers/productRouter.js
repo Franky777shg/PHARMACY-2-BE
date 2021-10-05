7076c84918f250a42b5452dcac002a193ff3f06a
EnricoGiovanno8 <genrico189@gmail.com>
2021-10-05 13:17:45 +0700

    As a user, I want to be able to filter the products list by its name, and/or category

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -3,5 +3,6 @@ const router = require('express').Router()
 const { productController } = require('../controllers')
 
 router.post('/get-product', productController.getProduct)
+router.post('/filter-product', productController.filterProduct)
 
 module.exports = router
