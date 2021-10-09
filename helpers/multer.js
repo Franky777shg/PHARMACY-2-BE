63b95a4c75ccc11d87e8f3609132a603280f4ac1
nicolesadjoli <nicole.sadjoli@gmail.com>
2021-10-09 12:08:31 +0700

    As an admin, I want to delete a product's name, price, category, and its picture

--- helpers/multer.js
+++ helpers/multer.js
@@ -17,20 +17,20 @@ module.exports = {
         })
         return multer({storage}).single('IMG') //.array --> klo mau banyak
         
-    }
-//   uploadApa : () => {
-//         let storage = multer.diskStorage({
-//             destination: path.join(path.resolve('public'), 'images', 'namaFoldernyaApa'), //tempat penyimpanan
-//             // localhost:2000/image
+    },
+  uploadp1 : () => {
+        let storage = multer.diskStorage({
+            destination: path.join(path.resolve('public'), 'images', 'produk_satuan'), //tempat penyimpanan
+            // localhost:2000/image
             
-//             filename : (req, file, cb) => {
-//                 cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname)) //eror, name
-//                 //IMG-16453271.png
+            filename : (req, file, cb) => {
+                cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname)) //eror, name
+                //IMG-16453271.png
 
-//             } 
-//             //rename file, req: nampung request, file : nyimpann file, cb : template yg digunakan
-//         })
-//         return multer({storage}).single('IMG') //.array --> klo mau banyak
+            } 
+            //rename file, req: nampung request, file : nyimpann file, cb : template yg digunakan
+        })
+        return multer({storage}).single('IMG') //.array --> klo mau banyak
         
-//     }
+    }
 }
