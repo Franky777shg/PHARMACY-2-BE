4bf1bc620ca307c95b8b770fdf9afb3b5f589b63
nafis-tech <nafiezlela9@gmail.com>
2021-10-18 14:30:36 +0700

    P2-36-filter-user-transaction

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,7 +3,10 @@ const { db } = require('../database')
 module.exports = {
     getTransaksiResep: (req, res) => {
         const { page } = req.body
-        let getOrderResep = 'select * from order_resep;'
+        // let getOrderResep = 'select * from order_resep;'
+        let getOrderResep = `select * from user u
+        join order_resep r
+        on u.iduser = r.iduser`
 
         db.query(getOrderResep, (err, resultSemuaOrder) => {
             if (err) {
@@ -18,7 +21,9 @@ module.exports = {
             let offset = (page * 5) - 5 //1-5 halaman
             // console.log(offset) //0
 
-            let getQuery = `select * from order_resep limit 5 offset ${offset};`
+            let getQuery = `select * from user u
+            join order_resep r
+            on u.iduser = r.iduser limit 5 offset ${offset};`
 
             db.query(getQuery, (err, resultPagination) => {
                 if (err) {
@@ -32,9 +37,12 @@ module.exports = {
     },
     getTransaksiSatuan: (req, res) => {
         const { page } = req.body
-        let getOrderResep = 'select * from order_satuan;'
+        // let getOrderSatuan = 'select * from order_satuan;'
+        let getOrderSatuan = `select * from user u
+        join order_satuan s
+        on u.iduser = s.iduser;`
 
-        db.query(getOrderResep, (err, resultSemuaOrderSatuan) => {
+        db.query(getOrderSatuan, (err, resultSemuaOrderSatuan) => {
             if (err) {
                 console.log(err)
                 res.status(400).send(err)
@@ -44,7 +52,9 @@ module.exports = {
             let maxPage = Math.ceil(jumlahSemuaOrder / 5)
             let offset = (page * 5) - 5 //1-5 halaman
 
-            let getQuery = `select * from order_satuan limit 5 offset ${offset};`
+            let getQuery = `select * from user u
+            join order_satuan s
+            on u.iduser = s.iduser limit 5 offset ${offset};`
 
             db.query(getQuery, (err, resultPaginationSatuan) => {
                 if (err) {
@@ -56,5 +66,73 @@ module.exports = {
             })
         })
     },
-    
+    filterTransByResep: (req, res) => {
+        const { name, page} = req.body
+        // console.log(`filterByName`)
+        let filterByName = `select * from user u
+            join order_resep r
+            on u.iduser = r.iduser
+            where username like '%${name}%' or date like '%${name}%' or order_number like '%${name}%';`
+
+        db.query(filterByName, (err, resultFilterByName) => {
+            if (err) {
+                console.log(err)
+                res.status(400).send(err)
+            }
+
+            let jumlahSemuaProdukBoth = resultFilterByName.length
+            let maxPageName = Math.ceil(jumlahSemuaProdukBoth / 5)
+            let offsetName = (page * 5) - 5
+
+            let paginationName = `select * from user u
+                join order_resep r
+                on u.iduser = r.iduser
+                where username like '%${name}%' or date like '%${name}%' or order_number like '%${name}%' limit 5 offset ${offsetName};`
+
+            db.query(paginationName, (err, resultPaginationName) => {
+                if (err) {
+                    console.log(err)
+                    res.status(400).send(err)
+                }
+
+                res.status(200).send([...resultPaginationName, maxPageName])
+            })
+        })
+
+    },
+    filterTransBySatuan: (req, res) => {
+        const { name, page } = req.body
+        // console.log(`filterByName`)
+        let filterBysatuan = `select * from user u
+            join order_satuan s
+            on u.iduser = s.iduser
+            where username like '%${name}%' or date like '%${name}%' or order_number like '%${name}%';`
+
+        db.query(filterBysatuan, (err, resultFilterBySatuan) => {
+            if (err) {
+                console.log(err)
+                res.status(400).send(err)
+            }
+
+            let jumlahSemuaFilter = resultFilterBySatuan.length
+            let maxPageSatuan = Math.ceil(jumlahSemuaFilter / 5)
+            let offsetSatuan = (page * 5) - 5
+
+            let paginationSatuan = `select * from user u
+                join order_satuan s
+                on u.iduser = s.iduser
+                where username like '%${name}%' or date like '%${name}%' or order_number like '%${name}%' limit 5 offset ${offsetSatuan};`
+
+            db.query(paginationSatuan, (err, resultPaginationSatuan) => {
+                if (err) {
+                    console.log(err)
+                    res.status(400).send(err)
+                }
+
+                res.status(200).send([...resultPaginationSatuan, maxPageSatuan])
+            })
+        })
+
+    },
+
 }
--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -4,5 +4,7 @@ const {adminController} = require('../controllers')
 
 router.post('/order-resep', adminController.getTransaksiResep)
 router.post('/order-satuan', adminController.getTransaksiSatuan)
+router.post('/filter-resep', adminController.filterTransByResep)
+router.post('/filter-satuan', adminController.filterTransBySatuan)
 
 module.exports = router
