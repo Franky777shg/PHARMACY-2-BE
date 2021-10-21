dafcaed4e0cf844c1c55fff36ba0e2ce2dafa58c
nafis-tech <nafiezlela9@gmail.com>
2021-10-21 14:24:22 +0700

    P2-28-revenue-filter-admin

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -6,5 +6,18 @@ router.post('/order-resep', adminController.getTransaksiResep)
 router.post('/order-satuan', adminController.getTransaksiSatuan)
 router.post('/filter-resep', adminController.filterTransByResep)
 router.post('/filter-satuan', adminController.filterTransBySatuan)
+router.post('/reven-resep', adminController.revenueResep)
+router.post('/reven-satuan', adminController.revenueSatuan)
+router.post('/total-revresep', adminController.totalRevenueResep)
+router.post('/total-revsatuan', adminController.totalRevenueSatuan)
+
+//REVENUE-resep-perbulan
+router.post('/reven-july', adminController.revenueResJuly)
+router.post('/total-revjuly', adminController.totalRevResJuly)
+
+//REVENUE-satuan-perbulan
+router.post('/satuan-july', adminController.revenueSatJuly)
+router.post('/total-satjuly', adminController.totalRevSatJuly)
+
 
 module.exports = router
