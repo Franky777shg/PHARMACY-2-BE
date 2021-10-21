9f54a36bf5cebcf1bcc791b871e0df1ccfb88752
nicolesadjoli <nicole.sadjoli@gmail.com>
2021-10-21 14:32:51 +0700

    p2-32 + p2-31

--- controllers/userhistoryController.js
+++ controllers/userhistoryController.js
@@ -13,8 +13,8 @@ module.exports = {
     inner join order_detail_satuan b
     on a.order_number=b.order_number
     where a.iduser=${req.params.iduser} AND a.status='cart';`
-    let posthistory = `update order_satuan set status='Waiting For Payment' where order_number=${db.escape(req.body.order_number)}`
-    let postpayment = `insert into payment_satuan (order_number, payment_proof_satuan,total_belanja,iduser) values (${db.escape(req.body.order_number)}, 'Waiting for Payment',${db.escape(req.body.total_bayar)},${req.params.iduser})`
+        let posthistory = `update order_satuan set status='Waiting For Payment' where order_number=${db.escape(req.body.order_number)}`
+
         db.query(getCart, (err, result) => {
             if (err) {
                 console.log(err)
@@ -22,19 +22,19 @@ module.exports = {
             }
             // res.status(200).send({cart: result})
             //edit ordernumber status ubah dr cart jd ongoing
-            
+
             db.query(posthistory, (err1, result1) => {
                 if (err1) {
                     console.log(err1)
                     res.status(400).send(err1)
                 }
                 console.log(result1)
-                db.query(postpayment, (err2, result2) => {
-                    res.status(200).send({ ongoing: result2 })
-                console.log(result2)
-                })
+                res.status(200).send({ ongoing: result1 })
+
+
             })
         })
+
     },
     getHistoryComplete: (req, res) => {
         let posthistoryComplete = `select a.order_number,b.idproduk,b.nama,b.product_image,b.qty_beli,b.harga,b.total_harga,a.status,a.date
@@ -48,34 +48,6 @@ module.exports = {
                 res.status(400).send(err)
             }
             res.status(200).send({ history: res })
-        })
-    },
-    getOGWaitingForPaymentApproval: (req, res) => {
-        console.log(req.body)
-        console.log(req.params.iduser)
-        // const {iduser} = req.body
-        //ambil ordernumber status cart
-        let getCart = `select a.order_number,b.idproduk,b.nama,b.product_image,b.qty_beli,b.harga,b.total_harga,a.status,a.date
-    from order_satuan a
-    inner join order_detail_satuan b
-    on a.order_number=b.order_number
-    where a.iduser=${req.params.iduser} AND a.status='Waiting For Payment';`
-        db.query(getCart, (err, result) => {
-            if (err) {
-                console.log(err)
-                res.status(400).send(err)
-            }
-            // res.status(200).send({cart: result})
-            //edit ordernumber status ubah dr cart jd ongoing
-            let posthistory = `update order_satuan set status='Waiting For Payment Approval' where order_number=${result.data.order_number}`
-            db.query(posthistory, (err1, result1) => {
-                if (err1) {
-                    console.log(err1)
-                    res.status(400).send(err1)
-                }
-                res.status(200).send({ ongoing: result1 })
-                console.log(result1)
-            })
         })
     },
     getOGProcessing: (req, res) => {
@@ -167,7 +139,13 @@ module.exports = {
         // console.log(req.params.iduser)
         // const {iduser} = req.body
         //ambil ordernumber status cart
+
         let getonGoing = `select a.order_number,b.idproduk,b.nama,b.product_image,b.qty_beli,b.harga,b.total_harga,a.status,a.date
+        from order_satuan a
+        inner join order_detail_satuan b
+        on a.order_number=b.order_number
+        where a.status IN ('Waiting For Payment', 'Waiting For Payment Approval', 'Sending Package', 'Processing') AND a.iduser=${req.params.iduser};`
+ let total_bayar = `select a.order_number,b.idproduk,b.nama,b.product_image,b.qty_beli,b.harga,b.total_harga,a.status,a.date, sum(b.total_harga) as total_bayar
         from order_satuan a
         inner join order_detail_satuan b
         on a.order_number=b.order_number
@@ -178,6 +156,7 @@ module.exports = {
                 res.status(400).send(err)
             }
             // res.status(200).send({hasilongoing: result})
+            // console.log(result[0].total_bayar)
             res.status(200).send(result)
         })
     },
@@ -198,19 +177,128 @@ module.exports = {
             }
             // res.status(200).send({hasilhistory: result})
             res.status(200).send(result)
-            })
-    
-    },
-    uploadpmt:(req,res) => {
-        console.log('req.file')
+        })
 
+    },
+    postpmt: (req, res) => {
+        console.log(req.body)
+        let getCartTotal = `select a.order_number,b.idproduk,b.nama,b.product_image,b.qty_beli,b.harga,b.total_harga,a.status, a.date, sum(b.total_harga) as total_bayar
+        from order_satuan a
+        inner join order_detail_satuan b
+        on a.order_number=b.order_number
+        where a.iduser=${req.params.iduser} AND a.status='Waiting for Payment';`
+
+        
+        // let postupdate = `update order_satuan set status='Waiting For Payment Approval' where order_number=${db.escape(req.body.order_number)} and iduser=${req.params.iduser}`
+
+        db.query(getCartTotal, (err, result) => {
+            if (err) {
+                console.log(err)
+                res.status(400).send(err)
+            }
+            // res.status(200).send({cart: result[0]})
+            console.log(result)
+            let postpayment = `insert into payment_satuan (order_number, payment_proof_satuan,total_belanja,iduser) values (${db.escape(result[0].order_number)}, 'Loading',${db.escape(result[0].total_bayar)},${req.params.iduser})`
+            db.query(postpayment, (err2, result2) => {
+                console.log(result2)
+                if (err2) {
+                    console.log(err2)
+                    res.status(400).send(err2)
+                }
+                // db.query(postupdate, (err1, result1) => {
+                //     if (err1) {
+                //         console.log(err1)
+                //         res.status(400).send(err1)
+                //     }
+                    res.status(200).send({ ongoing: result2 })
+                    // console.log(result1)
+                // })
+
+                // let objhistory = {
+                //     iduser: this.props.iduser,
+                //     username: this.props.username,
+                //     date: new Date().toLocaleString(),
+                //     isicart: this.state.rendercart,
+                //     order_number: result2.order_number,
+                //     total_bayar: this.state.total_bayar
+                //     // isicart: this.props.cart,
+                // }
+            })
+        })
+    },
+    uploadpmt: (req, res) => {
+        console.log('req.file')
+        console.log(req.file)
+        console.log(req.body)
         if (!req.file) {
             res.status(400).send('NO FILE')
         }
+        let proof = `${req.file.filename}`
 
+        let getCartTotal = `select a.order_number,b.idproduk,b.nama,b.product_image,b.qty_beli,b.harga,b.total_harga,a.status, sum(b.total_harga) as total_bayar
+        from order_satuan a
+        inner join order_detail_satuan b
+        on a.order_number=b.order_number
+        where a.iduser=${req.params.iduser} AND a.status='Waiting For Payment';`
+
+        let iduser = `${req.params.iduser}`
+        db.query(getCartTotal, (err1, res1) => {
+            console.log(res1)
+            if (err1) {
+                console.log(err1)
+                res.status(400).send(err1)
+            }
+            let postfotopmt = `update payment_satuan set payment_proof_satuan = ${proof} where order_number= ${db.escape(res1[0].order_number)} AND iduser =${iduser}`
+            db.query(postfotopmt, (err, result) => {
+                if (err) {
+                    console.log(err)
+                    res.status(400).send(err)
+                }
+                console.log(result)
+                res.status(200).send({ ongoing: result })
+            })
+        })
     },
-    checkuploadpmt: (req,res) => {
-        //cek dlu dia upload ga
-        let checkupload = `select`
+    gettotalBayar: (req, res) => {
+        console.log(req.body)
+        let getCartTotal = `select a.order_number,b.idproduk,b.nama,b.product_image,b.qty_beli,b.harga,b.total_harga,a.status, sum(b.total_harga) as total_bayar
+        from order_satuan a
+        inner join order_detail_satuan b
+        on a.order_number=b.order_number
+        where a.iduser=${req.params.iduser} AND a.order_number= ${db.escape(req.body.order_number)} AND a.status='Waiting for Payment';`
+        db.query(getCartTotal, (err, result) => {
+            if (err) {
+                console.log(err)
+                res.status(400).send(err)
+            }
+            res.status(200).send(result)
+            // res.status(200).send({cart: result[0]})
+            console.log(result)
+        })
+    },
+    getOGWaitingForPaymentApproval: (req,res) => {
+            let getCart = `select a.order_number,b.idproduk,b.nama,b.product_image,b.qty_beli,b.harga,b.total_harga,a.status,a.date
+        from order_satuan a
+        inner join order_detail_satuan b
+        on a.order_number=b.order_number
+        where a.iduser=${req.params.iduser} AND a.status='Waiting For Payment';`
+            db.query(getCart, (err, result) => {
+                if (err) {
+                    console.log(err)
+                    res.status(400).send(err)
+                }
+                // res.status(200).send({cart: result})
+                //edit ordernumber status ubah dr cart jd ongoing
+                let posthistory = `update order_satuan set status='Waiting For Payment Approval' where order_number=${result.data.order_number}`
+                db.query(posthistory, (err1, result1) => {
+                    if (err1) {
+                        console.log(err1)
+                        res.status(400).send(err1)
+                    }
+                    res.status(200).send({ ongoing: result1 })
+                    console.log(result1)
+                })
+            })
     }
+    
 }
--- routers/userhistoryRouter.js
+++ routers/userhistoryRouter.js
@@ -4,7 +4,7 @@ const { userhistoryController } = require('../controllers')
 const { uploadp1,uploadpr,uploadPaymentSatuan } = require('../helpers/multer')
 const uploader = uploadp1()
 const uploader1 = uploadpr()
-const uploaderpmt = uploadPaymentSatuan
+const uploaderpmt = uploadPaymentSatuan()
 
 router.post('/og-wfp/:iduser', userhistoryController.OGWaitingforPayment)
 router.post('/og-wfpa/:iduser', userhistoryController.getOGWaitingForPaymentApproval)
@@ -14,7 +14,8 @@ router.post('/og-hist/:iduser', userhistoryController.getOGHist)
 router.post('/h-complete/:iduser', userhistoryController.getHistoryComplete)
 router.get('/get-ongoing/:iduser', userhistoryController.getonGoing)
 router.get('/get-history/:iduser', userhistoryController.getHistory)
-router.patch('/upload-pmt/:iduser', uploaderpmt, userhistoryController.uploadpmt)
-// router.post('/post-pmt/:iduser', userhistoryController.postpmt)
+router.post('/upload-pmt/:iduser', uploaderpmt, userhistoryController.uploadpmt)
+router.post('/get-totalbayar/:iduser', userhistoryController.gettotalBayar)
+router.post('/post-pmt/:iduser', userhistoryController.postpmt)
 
 module.exports = router
